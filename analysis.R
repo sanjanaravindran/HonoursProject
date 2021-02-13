bf6b90d31be7b6f21c04b16d98557e35c07c8c39
sanjanaravindran <sanjana.r199@gmail.com>
2021-02-13 13:33:27 +0000

    I added a comment

--- analysis.R
+++ analysis.R
@@ -1,4 +1,3 @@
-<<<<<<< HEAD
 # Install/Load libraries
 if (!require("pacman")) install.packages("pacman")
 pacman::p_load(here, usethis, ggplot2, reshape, dplyr, magrittr, cowplot, gdata, tidyr)
@@ -8,15 +7,6 @@ tl_para_lambs2011 <- read.csv(here("RTL_Parasites_Lambs2011.csv"))
 
 # Explore the data 
 ggplot(tl_para_lambs2011, aes(x=RTL)) +
-=======
-# Install/Load libraries
-if (!require("pacman")) install.packages("pacman")
-pacman::p_load(here, usethis, ggplot2, reshape, dplyr, magrittr, cowplot, gdata, tidyr)
-
-# Load the data 
-tl_para_lambs2011 <- read.csv(here("RTL_Parasites_Lambs2011.csv"))
-
-# Explore the data 
-ggplot(tl_para_lambs2011, aes(x=RTL)) +
->>>>>>> e9e648cbf921ae60063b973424b488f7bdf5f4f1
   geom_density()
+
+# I have added a new line
